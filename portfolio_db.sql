d4bda80228d836c8839f0cc36e6471b2a9bd0e27
Ryan Paulo Magnaye <magnaye.rp@gmail.com>
2026-01-06 23:12:13 +0800

    Create portfolio_db.sql

---
+++ portfolio_db.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 29, 2025 at 05:32 AM
+-- Server version: 11.8.3-MariaDB-log
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `u768452900_portfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_groups_users`
+--
+-- Error reading structure for table u768452900_portfolio.auth_groups_users: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`auth_groups_users`
+-- Error reading data for table u768452900_portfolio.auth_groups_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`auth_groups_users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_identities`
+--
+-- Error reading structure for table u768452900_portfolio.auth_identities: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`auth_identities`
+-- Error reading data for table u768452900_portfolio.auth_identities: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`auth_identities`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_logins`
+--
+-- Error reading structure for table u768452900_portfolio.auth_logins: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`auth_logins`
+-- Error reading data for table u768452900_portfolio.auth_logins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`auth_logins`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_permissions_users`
+--
+-- Error reading structure for table u768452900_portfolio.auth_permissions_users: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`auth_permissions_users`
+-- Error reading data for table u768452900_portfolio.auth_permissions_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`auth_permissions_users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_remember_tokens`
+--
+-- Error reading structure for table u768452900_portfolio.auth_remember_tokens: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`auth_remember_tokens`
+-- Error reading data for table u768452900_portfolio.auth_remember_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`auth_remember_tokens`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_token_logins`
+--
+-- Error reading structure for table u768452900_portfolio.auth_token_logins: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`auth_token_logins`
+-- Error reading data for table u768452900_portfolio.auth_token_logins: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`auth_token_logins`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `certificates`
+--
+-- Error reading structure for table u768452900_portfolio.certificates: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`certificates`
+-- Error reading data for table u768452900_portfolio.certificates: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`certificates`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table u768452900_portfolio.migrations: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`migrations`
+-- Error reading data for table u768452900_portfolio.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`migrations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_images`
+--
+-- Error reading structure for table u768452900_portfolio.project_images: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`project_images`
+-- Error reading data for table u768452900_portfolio.project_images: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`project_images`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_list`
+--
+-- Error reading structure for table u768452900_portfolio.project_list: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`project_list`
+-- Error reading data for table u768452900_portfolio.project_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`project_list`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+-- Error reading structure for table u768452900_portfolio.settings: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`settings`
+-- Error reading data for table u768452900_portfolio.settings: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`settings`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table u768452900_portfolio.users: #1142 - SHOW command denied to user 'u768452900_aquaman'@'127.0.0.1' for table `u768452900_portfolio`.`users`
+-- Error reading data for table u768452900_portfolio.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `u768452900_portfolio`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
